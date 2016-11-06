897d2cd552cf3f15ff54201d686a33088dafcb86
LY9 <821931356@qq.com>
2016-11-06 23:55:30 +0800

    文件操作练习

--- app/src/main/java/com/ly9/diary/record/MainActivity.java
+++ app/src/main/java/com/ly9/diary/record/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ly9.diary.record;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView mRecycler=(RecyclerView)findViewById(R.id.recycler_1);
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
         mRecycler.setAdapter(new MyReAdapter(this));
+        createSDCardDir();
+    }
+
+    //在SD卡上创建一个文件夹
+    public void createSDCardDir(){
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+            // 创建一个文件夹对象，赋值为外部存储器的目录
+            File sdcardDir =Environment.getExternalStorageDirectory();
+            //得到一个路径，内容是sdcard的文件夹路径和名字
+            String path=sdcardDir.getPath()+"/cardImages";
+            File path1 = new File(path);
+            if (!path1.exists()) {
+                //若不存在，创建目录，可以在应用启动的时候创建
+                path1.mkdirs();
+                setTitle("paht ok,path:"+path);
+            }
+        }
+        else{
+            setTitle("false");
+            return;
+
+        }
 
     }
 }
